6d8b0e9b0b8f9af786e5bd68e25920a4187e1432
Larry Li <larrylisky@gmail.com>
2015-09-13 13:03:20 -0500

    Add setup.h

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export VOXEL_FW_PATH=/home/larry/3dtof/CalculusDemo/voxel-sdk/libti3dtof/build/TI3DToF
+export VOXEL_CONF_PATH=/home/larry/3dtof/CalculusDemo/voxel-sdk/libti3dtof/build/TI3DToF
+export VOXEL_LIB_PATH=/home/larry/3dtof/CalculusDemo/voxel-sdk/libti3dtof/build/TI3DToF
+
