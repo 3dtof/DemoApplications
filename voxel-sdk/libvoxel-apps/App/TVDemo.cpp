5accd2a6cafa8180a15071f184bc85ba82ed4c26
Larry Li <larrylisky@gmail.com>
2015-09-15 09:37:05 -0500

    Fixed a bug in VoxelPCLConfig.cmake.in and added a working TVDemo.cpp that uses USB-UIRT to turn on-off TV by presence

--- voxel-sdk/libvoxel-apps/App/TVDemo.cpp
+++ voxel-sdk/libvoxel-apps/App/TVDemo.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2014 Texas Instruments Inc.
  */
+#include <cstdlib>
 #include <deque>
 #include <CameraSystem.h>
 #include <Common.h>
@@ -16,11 +17,15 @@ using namespace std;
 using namespace Voxel;
 using namespace pcl::visualization;
 
-#define XDIM		80 
-#define YDIM		60 
+#define XDIM		(80) 
+#define YDIM		(60) 
+#define FIFO_SIZE	(3)
+#define DELAY_COUNT	(250)
 
 deque < DepthFrame > qFrame;
 DepthFrame *frm;
+DepthFrame *frmbuf;
+DepthFrame *frmbknd;
 
 int getkey();
 
@@ -52,13 +57,53 @@ int getkey() {
 void frameCallback(DepthCamera &dc, const Frame &frame, 
                    DepthCamera::FrameType c)
 {
-   if (qFrame.size() < 2) 
+   if (qFrame.size() < FIFO_SIZE) 
    {
       const DepthFrame* f = dynamic_cast< const DepthFrame* > (&frame);
       qFrame.push_back(*f);
    }
 }
 
+DepthFrame *InitDepthFrame(DepthFrame *f, int x_sz, int y_sz)
+{
+  for (int i = 0; i < x_sz*y_sz; i++) 
+  {
+     f->depth.push_back(0);
+     f->amplitude.push_back(0);
+  }
+  f->size.width = y_sz;
+  f->size.height = x_sz;
+  return f;
+}
+
+
+DepthFrame *iirFilter(DepthFrame *f, float coef)
+{
+   for (int i = 0; i < f->size.width * f->size.height; i++) 
+   {
+      frmbuf->depth[i] = (1.0f - coef) * (frmbuf->depth[i])
+                        + coef * (f->depth[i]);
+   }
+   return frmbuf;
+}
+
+bool detectObject(float dist)
+{
+#if 0
+   for (int x = XDIM/8; x < XDIM-XDIM/8; x++) {
+      for (int y = YDIM/8; y < YDIM-YDIM/8; y++) { 
+         int i = XDIM*y+x;
+         if (frmbuf->depth[i] < dist && fabs(frmbknd->depth[i] - frmbuf->depth[i]) > 0.3) 
+             return true;
+      }
+   }
+#else
+   int i = XDIM*(YDIM/2)+XDIM/2;
+   if (frmbuf->depth[i] < dist && fabs(frmbknd->depth[i] - frmbuf->depth[i]) > 0.3) 
+       return true;
+#endif
+   return false;
+}
 
 int main (int argc, char* argv[])
 {
@@ -68,6 +113,14 @@ int main (int argc, char* argv[])
   CameraSystem sys;
   DepthCameraPtr depthCamera;
   
+
+  if (argc != 2) {
+     cout << "Usage: sudo TVDemo <dist>" << endl;
+     exit(1);
+  }
+  
+  float dist = atof(argv[1]);
+
   const Vector<DevicePtr> &devices = sys.scan();
   
   if(devices.size() > 0) {
@@ -93,6 +146,10 @@ int main (int argc, char* argv[])
  
   cout << "Successfully loaded depth camera " << endl; 
   
+
+  frmbuf = InitDepthFrame(new DepthFrame(), XDIM, YDIM);
+  frmbknd = InitDepthFrame(new DepthFrame(), XDIM, YDIM);
+
   depthCamera->registerCallback(DepthCamera::FRAME_DEPTH_FRAME, frameCallback);
 
   iv = new pcl::visualization::ImageViewer("Depth");
@@ -100,22 +157,63 @@ int main (int argc, char* argv[])
 
   depthCamera->start();
 
-  while ('q' != getkey()) {
+  bool done = false;
+  bool prevDetected = false;
+  bool init = false;
+  bool background_init = false;
+  int count = 0;
+  int tv_count = DELAY_COUNT; 
+
+  while (!done) {
+  
+      int key = getkey();
+      if (key == 'q') done = true;
 
       if (!qFrame.empty()) {
 
          frm = &qFrame.front(); 
-         
+         int index = YDIM*(XDIM/2)+YDIM/2;
+         iirFilter(frm, 0.1f);
+         cout << frmbuf->depth.data()[index] << endl;
+
          unsigned char *rgb = FloatImageUtils::getVisualImage(
-                           frm->depth.data(), XDIM, YDIM, 0, 4);
-         #iv->showRGBImage(rgb, XDIM, YDIM); 
-         iv->showRGBImage(rgb, 320, 240); 
+                              frmbuf->depth.data(), XDIM, YDIM, 0, 6);
+         iv->showRGBImage(rgb, XDIM, YDIM); 
          delete rgb;
+
+         if (count > 30) {
+            init = true;
+            cout << "init = true" << endl;
+            if (!background_init) {
+               *frmbknd = *frmbuf;
+               background_init = true;
+               cout << "background_init = true" << endl;
+            }
+         }
+         else 
+            count++; 
 
          qFrame.pop_front();
       }
 
-      usleep(100000);
+      if (background_init) {
+         if (detectObject(dist)) {
+            cout << "People in Range" << endl;
+            tv_count = DELAY_COUNT; 
+            if (!prevDetected) {
+               system("irsend -d /var/run/lirc/lircd1 SEND_ONCE samsung.conf KEY_POWER");
+               prevDetected = true;
+            }
+         }
+         else {
+            cout << "..............." << endl;
+            if (prevDetected && tv_count-- == 0) { 
+                system("irsend -d /var/run/lirc/lircd1 SEND_ONCE samsung.conf KEY_POWER");
+                prevDetected = false; 
+            }
+         }
+      }  
+      usleep(66000);
 
   } // end while 
 
--- voxel-sdk/libvoxel-apps/setup.sh
+++
@@ -1,3 +0,0 @@
-export VOXEL_FW_PATH=/home/larry/Software/voxel-sdk/libti3dtof/build/TI3DToF
-export VOXEL_CONF_PATH=/home/larry/Software/voxel-sdk/libti3dtof/build/TI3DToF
-export VOXEL_LIB_PATH=/home/larry/Software/voxel-sdk/libti3dtof/build/TI3DToF
